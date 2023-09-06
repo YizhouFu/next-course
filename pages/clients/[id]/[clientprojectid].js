63ac991a78dda284db9c7ca7ceaa533f1652cf73
JadFu <yfu5@aceproject.space>
2023-09-06 15:24:28 -0500

    init commit
    first commit, best commit

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,9 @@
+function specificProjectPage() {
+    return (
+      <div>
+        <h1>the specific project of a given client</h1>
+      </div>
+    );
+  }
+  
+  export default specificProjectPage;
---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,9 @@
+function ClientProjectPage() {
+  return (
+    <div>
+      <h1>the project of a given client</h1>
+    </div>
+  );
+}
+
+export default ClientProjectPage;
