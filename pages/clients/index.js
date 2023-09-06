36aad2060b47d9bd8e12a5f9bd1ae7cd65efd660
JadFu <yfu5@aceproject.space>
2023-09-06 16:01:52 -0500

    learning different routing

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,35 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "min", name: "Minimilian" },
+  ];
+
   return (
     <div>
       <h1>the client page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <ol>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
