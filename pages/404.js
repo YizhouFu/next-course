604bbfd9c9b9607491c876337b9c7c1d5b706321
JadFu <yfu5@aceproject.space>
2023-09-07 08:43:02 -0500

    finishing routing

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+function PageNotFound() {
+  return (
+    <div>
+      <h1>This Page Cannot Be Found Or Does Not Exsit</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,21 @@
+import { useRouter } from "next/router";
+
 function ClientProjectPage() {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  function handleLoadProject(){
+    //router.replace: user cannot go back to this page after click
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: {id: 'max', clientprojectid: 'projecta'}
+    });
+  }
   return (
     <div>
       <h1>the project of a given client</h1>
+      <button onClick={handleLoadProject}>Load Project A</button>
     </div>
   );
 }
